6dec08198d22a8129c6809403f5ebba8fcb2949c
John Osaze <johnfavourosaze@gmail.com>
2022-11-10 23:54:42 +0100

    Rearranged files for deploying

--- src/App.js
+++ src/App.js
@@ -1,48 +1,140 @@
-import React, { createContext, useEffect, useState } from 'react'
-import { Route, Routes, useLocation } from "react-router-dom";
-import PageNotFound from './components/PageNotFound';
-import Header from './components/Header';
-import Home_Page from './pages/Home_Page';
-import Landing_Page from './Landing_Page'
-import Signup from './pages/Signup';
-import Login from './pages/Login';
+import { Container, Row, Col, Button } from 'reactstrap';
+import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import ServiceIcon1 from "./assets/images/service-icon-1.png";
+import ServiceIcon2 from "./assets/images/service-icon-2.png";
+import ServiceIcon3 from "./assets/images/service-icon-3.png";
+import ServiceIcon4 from "./assets/images/service-icon-4.png";
+import About from './components/About/About';
+import HowItWorks from './components/How-It-Works/HowItWorks';
+import WhyChooseUs from './components/Why-Choose-Us/WhyChooseUs';
+import { Fragment } from 'react';
+// import Testimonial from './components/Testimonial/Testimonial';
+// import Team from './components/Team/Team';
+// import Trustee1 from "./assets/images/trustee-1.png";
+// import Trustee2 from "./assets/images/trustee-2.png";
+// import Trustee3 from "./assets/images/trustee-3.png";
+// import Trustee4 from "./assets/images/trustee-4.png";
 
-export const StateContext = createContext();
+// import {Routes} from 'react-router-dom'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [showHeader, setShowHeader] = useState(true);
-  const [user, setUser] = useState({})
-  const [mediData, setMediData] = useState([]);
-  
-  const noUrl = ['404', 'login', 'signup']
-  const location = useLocation();
-  useEffect(()=>{
-    setShowHeader(true)
-    // console.log(location.pathname)
-    noUrl.forEach(url=>{
-      if (location.pathname.search(url)!=-1) {
-        return setShowHeader(false)
-      }
+	const gridItems = [
+		{
+			img: ServiceIcon1,
+			title: "Complete",
+			text: "All your records in one place guaranteed"
+		},
 
-    })
-  },[location])
-  return (
-    <StateContext.Provider value={{isLoggedIn, user, setUser, mediData, setMediData}}>
-      {/* {showHeader && <Header />} */}
-      <Routes>
-          {/* <Route exact path='/login' element={<Login />}/> */}
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/home/*' element={<Home_Page />} />
-          {/* <Route path='/add' element={<AddForm />} /> */}
-          <Route exact path='/' element={<Landing_Page />}/>
-          <Route path='/404' element={<PageNotFound/>} />
-          <Route path='*' element={<PageNotFound/>} />
-          
-      </Routes>
-    </StateContext.Provider>
-);
+		{
+			img: ServiceIcon2,
+			title: "Complete",
+			text: "All your records in one place guaranteed"
+		},
+
+		{
+			img: ServiceIcon3,
+			title: "Complete",
+			text: "All your records in one place guaranteed"
+		},
+	];
+
+	const privateDataGrid = [
+		{
+			icon: ServiceIcon1,
+			title: "HIPAA compliant",
+			text: "We follow the highest standard set by the law"
+		},
+
+		{
+			icon: ServiceIcon2,
+			title: "HIPAA compliant",
+			text: "We follow the highest standard set by the law"
+		},
+
+		{
+			icon: ServiceIcon3,
+			title: "HIPAA compliant",
+			text: "We follow the highest standard set by the law"
+		},
+
+		{
+			icon: ServiceIcon4,
+			title: "HIPAA compliant",
+			text: "We follow the highest standard set by the law"
+		}
+	]
+
+	return (
+		<Fragment>
+			<Header />
+			<Hero />
+			{/* <Container className="sponsors">
+				<div className="title">
+					Our app is trusted by established organizations
+				</div>
+				<Row>
+					<Col lg="3" md="4" sm="6">
+						<img src={Trustee1} alt=""/>
+					</Col>
+					<Col lg="3" md="4" sm="6">
+						<img src={Trustee2} alt=""/>
+					</Col>
+					<Col lg="3" md="4" sm="6">
+						<img src={Trustee3} alt=""/>
+					</Col>
+					<Col lg="3" md="4" sm="6">
+						<img src={Trustee4} alt=""/>
+					</Col>
+				</Row>
+			</Container> */}
+			<About />
+			<Container className='section'>
+				<section className="medical-showcase">
+					<h1 className='text-center heading'>Your Medical Data <br /> as it should be</h1>
+
+					<Container className="showcase-grid justify-content-center">
+						<Row className=" align-items-center">
+							{
+								gridItems.map((item, index) => <Col key={index} lg="4" md="6" sm="12" className='text-center'>
+								<img src={item.img} alt=""/>
+								<h5>{item.title}</h5>
+								<p>{item.text}</p>
+							</Col>)
+							}
+						</Row>
+					</Container>
+				</section>
+			</Container>
+			<WhyChooseUs/>
+			<HowItWorks/>
+			{/* <Team/> */}
+			<Container className='section data-privacy'>
+				<div className="title-area text-center">
+					<h1 className="title">Your private data stays private</h1>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, accusantium, et quasi, dignissimos aliquam consequatur natus labore animi ipsam expedita nihil. Consequuntur enim quisquam labore rerum sapiente sunt facere in.</p>
+				</div>		
+				<Row className='privacy-terms' >
+					{
+						privateDataGrid.map((item, index) => <Col lg="6" md="6" sm="12" key={index} className="d-flex align-items-center justify-content-center privacy-item">
+						<div className="icon">
+							<img src={item.icon} alt="" width={"50px"} height={"50px"}/>
+						</div>
+						<div className="info">
+							<h4>{item.title}</h4>
+							<p>{item.text}</p>
+						</div>
+					</Col>)
+					}
+				</Row>
+				<div className="text-center">
+					<Button color='primary'>Learn More</Button>
+				</div>
+			</Container>
+			{/* <Testimonial /> */}
+		</Fragment>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'remixicon/fonts/remixicon.css';
 import './index.css';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-document.getElementById("root")
-);
+import App from './App';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
